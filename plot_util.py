7872e36cc0a54f4cf213035974a1c05f1dea29d8
prometheusua <PrometheusUA@i.ua>
2022-11-29 16:24:47 +0200

    Plot for non-log graphs added

--- plot_util.py
+++ plot_util.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-EPS = 0.01
+EPS = 0.005
 
 def plot_fitness_function(*args: np.array):
     plt.title("Fitness function")
@@ -9,4 +9,12 @@ def plot_fitness_function(*args: np.array):
     plt.ylabel("log2(log2(value))")
     for j, values in enumerate(args):
         plt.plot([i for i in range(len(values))], np.log2(np.log2(values)) - EPS*j)
+    plt.show()
+
+def plot_fitness_function_unlogged(*args: np.array):
+    plt.title("Fitness function")
+    plt.xlabel("Iteration")
+    plt.ylabel("value")
+    for j, values in enumerate(args):
+        plt.plot([i for i in range(len(values))], values - EPS*j)
     plt.show()
