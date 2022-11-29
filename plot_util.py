601d81c55851b4e81098e2bdda59ab1bb4503f8e
prometheusua <PrometheusUA@i.ua>
2022-11-29 13:52:35 +0200

    Update for graphs to not overlap

--- plot_util.py
+++ plot_util.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+EPS = 0.01
 
 def plot_fitness_function(*args: np.array):
     plt.title("Fitness function")
     plt.xlabel("Iteration")
     plt.ylabel("log2(log2(value))")
-    for values in args:
-        plt.plot([i for i in range(len(values))], np.log2(np.log2(values)))
+    for j, values in enumerate(args):
+        plt.plot([i for i in range(len(values))], np.log2(np.log2(values)) - EPS*j)
     plt.show()
